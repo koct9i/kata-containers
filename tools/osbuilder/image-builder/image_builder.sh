cce06a486277b3b94666b4d2ae5ff28c25b4b67e
Konstantin Khlebnikov <koct9i@gmail.com>
2025-03-14 18:39:45 +0100

    ci: fix loop device leak

--- tools/osbuilder/image-builder/image_builder.sh
+++ tools/osbuilder/image-builder/image_builder.sh
@@ -321,7 +321,7 @@ setup_loop_device() {
 
 	# Get the loop device bound to the image file (requires /dev mounted in the
 	# image build system and root privileges)
-	device=$(losetup -P -f --show "${image}")
+	device=$(losetup -P -f --show "${image}") || die "Could not setup loop device"
 
 	#Refresh partition table
 	partprobe -s "${device}" > /dev/null
@@ -335,6 +335,7 @@ setup_loop_device() {
 	done
 
 	error "File ${device}p1 is not a block device"
+	losetup -d "${device}" || true
 	return 1
 }
 
